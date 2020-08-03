ed937f35301695e9f4e62f9e17436625709d2e40
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-08-03 18:06:18 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Run dotenv
+require('dotenv').config();
+
+// Import libraries
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Event listener when a user connected to the server.
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Event listener when a user sends a message in the chat.
+client.on('message', msg => {
+
+  // We check the message content and looks for the word "ping", so we can have the bot respond "pong"
+  if (msg.content === 'hello') {
+    msg.reply('Hi! Remember to Drink Water Today :)');
+  }
+
+});
+
+// Initialize bot by connecting to the server
+client.login(process.env.DISCORD_TOKEN);
