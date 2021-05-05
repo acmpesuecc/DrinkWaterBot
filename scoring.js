05a9656f26b1cc5cc9b60f3f0c667e7f62b4e227
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-05 23:17:07 +0530

    Refactor firebase functions

--- scoring.js
+++ scoring.js
@@ -8,55 +8,33 @@ const db = admin.firestore()
 
 // Function to increment the user's score
 async function incScore(userid, points) {
-    // try {
-    //     const userRef = db.collection('users').doc(userid);
-    //     await userRef.update({
-    //         score: admin.firestore.FieldValue.increment(points)
-    //     });
-    // }
-    // catch {
-    //     const data = {
-    //         score: 1
-    //     };
-    //     await db.collection('users').doc(userid).set(data);
-    // }
+    try {
+        const userRef = db.collection('users').doc(userid);
+        await userRef.update({
+            score: admin.firestore.FieldValue.increment(points)
+        });
+    }
+    catch {
+        const data = {
+            score: 1
+        };
+        await db.collection('users').doc(userid).set(data);
+    }
 }
 
 // Function to get the user's score
-// async function getScore(userid) {
-// const userRef = db.collection('users').doc(userid);
-// const doc = await userRef.get();
-// if (!doc.exists) {
-//     return 0;
-// } else {
-//     var points = await doc.data().score;
-//     return points;
-// }
-// }
+async function getScore(userid) {
+    const userRef = db.collection('users').doc(userid);
+    const doc = await userRef.get();
+    if (!doc.exists) {
+        return 0;
+    } else {
+        var points = await doc.data().score;
+        return points;
+    }
+}
 
 module.exports = {
-    inc: async function inscore(userid, points) {
-        try {
-            const userRef = db.collection('users').doc(userid);
-            await userRef.update({
-                score: admin.firestore.FieldValue.increment(points)
-            });
-        }
-        catch {
-            const data = {
-                score: 1
-            };
-            await db.collection('users').doc(userid).set(data);
-        }
-    },
-    get: async function getScore(userid) {
-        const userRef = db.collection('users').doc(userid);
-        const doc = await userRef.get();
-        if (!doc.exists) {
-            return 0;
-        } else {
-            var points = await doc.data().score;
-            return points;
-        }
-    }
+    inc: incScore,
+    get: getScore
 };
