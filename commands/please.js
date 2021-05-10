1da928ea97ba57afd0801960ca015a623a299c8e
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-10 12:11:29 +0530

    Pls get me out of happy

--- commands/please.js
+++ commands/please.js
@@ -5,6 +5,9 @@ function command(msg) {
         case "please get me out of sad":
             msg.channel.send('https://tenor.com/view/slap-bear-slap-me-you-gif-17942299');
             break;
+        case "please get me out of happy":
+            msg.channel.send('https://tenor.com/view/killed-em-hold-this-stabbed-gif-14017151');
+            break;
     }
 }
 
