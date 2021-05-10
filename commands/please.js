51e1e6790f130f361de976e3cc83acb885e99343
Karuna K <74588951+karunakc@users.noreply.github.com>
2021-05-10 12:40:39 +0530

    Update please.js

--- commands/please.js
+++ commands/please.js
@@ -8,6 +8,9 @@ function command(msg) {
         case "please get me out of happy":
             msg.channel.send('https://tenor.com/view/killed-em-hold-this-stabbed-gif-14017151');
             break;
+        case "please give me pats":
+            msg.channel.send('https://media0.giphy.com/media/mn1cym1jiJOUg/giphy.gif');
+            break;
     }
 }
 
