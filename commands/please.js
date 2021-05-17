2b357971f96c72a855f66e3a0b409380f49106a0
Karuna K <74588951+karunakc@users.noreply.github.com>
2021-05-17 21:41:00 +0530

    Update please.js

--- commands/please.js
+++ commands/please.js
@@ -14,20 +14,21 @@ function command(msg) {
             msg.channel.send('https://media0.giphy.com/media/mn1cym1jiJOUg/giphy.gif');
             scoring.inc(msg.author.id, 1);
             break;
-         case "please give me hugs":
-            msg.channel.send('https://media.tenor.com/images/ecf4840ba6fac22be773e586493d5283/tenor.gif');
+        case "please give me hugs":
+            msg.channel.send('https://media.tenor.com/images/2d34e964879615029896fe8f2e296feb/tenor.gif');
             scoring.inc(msg.author.id, 1);
-            break;
-         case "no tears please":
+            break;  
+        case "no tears please":
             msg.channel.send('https://i.pinimg.com/originals/3e/ce/ef/3eceefc04128c32e8a5f5173dd1bd27f.gif');
             scoring.inc(msg.author.id, 1);
-            break; 
-         case "good vibes only":
-            msg.channel.send('https://i.pinimg.com/564x/af/e4/c6/afe4c6614ea9c631059d941ed00b4ee5.jpg');
+            break;      
+        case "please give good vibes":
+            msg.channel.send('https://i.pinimg.com/originals/3e/ce/ef/3eceefc04128c32e8a5f5173dd1bd27f.gif');
             scoring.inc(msg.author.id, 1);
-            break;
+            break;      
             
     }
+    
 }
 
 
