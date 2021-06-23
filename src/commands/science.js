014397f99a7e1392da4b37afd8d75092b7efe83c
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-06-23 23:32:53 +0530

    Add basic map code

--- src/commands/science.js
+++ src/commands/science.js
@@ -1,4 +1,5 @@
 const { default: axios } = require('axios');
+const StaticMaps = require('staticmaps');
 const scoring = require('../scoring');
 
 function issLoc(msg) {
@@ -8,6 +9,25 @@ function issLoc(msg) {
         url: url,
     }).then((response) => {
         data = response.data;
+        const center = [0, 0];
+        const options = {
+            width: 600,
+            height: 400
+        };
+        const map = new StaticMaps(options);
+        const marker = {
+            img: `${__dirname}/../img/marker.png`, // can also be a URL,
+            offsetX: 240,
+            offsetY: 240,
+            width: 1024,
+            height: 1024,
+            coord: [data['iss_position']['latitude'], data['iss_position']['longitude']],
+        };
+        map.addMarker(marker);
+        map.render(center)
+            .then(() => map.image.save('single-marker.png'))
+            .then(() => { console.log('File saved!'); })
+            .catch(console.log);
         msg.reply(`The ISS is currently above ${data['iss_position']['latitude']}, ${data['iss_position']['longitude']}`);
     });
     scoring.inc(msg.author.id, 1);
