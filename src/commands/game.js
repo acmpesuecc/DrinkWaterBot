be68887d1f7d638dac78adf7e051e5d5de0e453f
Omicron <rsamal.b305@gmail.com>
2021-10-16 11:07:35 +0530

    Added dice size

--- src/commands/game.js
+++ src/commands/game.js
@@ -9,13 +9,16 @@ function docs() {
 
 function dice(msg) {
     var msgtok = msg.content.toLowerCase().split(" ");
+    var size
     if (!isNaN(msgtok[2])) {
+        size = `${msgtok[2]} sided `
         var rolled = Math.floor(Math.random() * parseInt(msgtok[2])) + 1;
     }
     else {
+        size = ""
         var rolled = Math.floor(Math.random() * 6) + 1;
     }
-    msg.reply(`The 🎲 rolls a ${rolled}`);
+    msg.reply(`The ${size} 🎲 rolls a ${rolled}`);
     scoring.inc(msg.author.id, 1);
 }
 
