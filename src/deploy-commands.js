ef19be68979bdb7971136d410e0e934f5f32410d
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-10-24 18:46:42 +0530

    Add global refresh and flags to deployment

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -7,7 +7,22 @@ const token = process.env.DWB_DISCORD_TOKEN;
 const guildId = process.env.GUILD_ID;
 const clientId = process.env.CLIENT_ID;
 
-console.log(token, guildId, clientId)
+var arg = process.argv.slice(2)[0];
+var scope = -1;
+
+if (arg === '--test' || arg === '--guild' || arg === '-t') {
+    scope = 0;
+}
+else if (arg === '--global' || arg === '-g') {
+    scope = 1;
+}
+else {
+    console.error(`error: unknown option \'${arg}\'`)
+    console.error(`usage: node src/deploy-commands.js [<option>]\n`)
+    console.error("\t-t, --test \t guild level testing")
+    console.error("\t-g, --global \t global deployment")
+    return;
+}
 
 const commands = [];
 
@@ -22,14 +37,22 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
+        if (scope == 0) {
+            console.log('Started refreshing guild application (/) commands.');
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: commands },
+            );
+            console.log('Successfully reloaded guild application (/) commands.');
+        }
+        else if (scope == 1) {
+            console.log('Started refreshing global application (/) commands.');
+            await rest.put(
+                Routes.applicationCommands(clientId),
+                { body: commands },
+            );
+            console.log('Successfully reloaded global application (/) commands.');
+        }
     } catch (error) {
         console.error(error);
     }
