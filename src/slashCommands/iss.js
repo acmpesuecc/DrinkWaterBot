cc3d2d2010ac4da055dce0c85cfc4fdcf806ad14
Sanskriti Pattanayak <48806637+sanskritip@users.noreply.github.com>
2021-10-29 01:34:23 +0530

    Add reverse geocoding to /iss (#146)
    * Added npm scripts for deployment
    
    * Added ISS location API
    
    * Refactor
    
    Co-authored-by: Srujan Deshpande <srujan.deshpande@gmail.com>

--- src/slashCommands/iss.js
+++ src/slashCommands/iss.js
@@ -9,38 +9,59 @@ module.exports = {
         .setName('iss')
         .setDescription("Current location of the ISS"),
     async execute(interaction) {
+        await interaction.reply('Checking...');
         scoring.inc(interaction.user.id, 1);
+
         const url = "http://api.open-notify.org/iss-now.json";
-        axios({
-            method: 'get',
-            url: url,
-        }).then((response) => {
-            data = response.data;
-            const lon = parseFloat(data['iss_position']['longitude']);
-            const lat = parseFloat(data['iss_position']['latitude']);
-            const coords = [lon, lat];
-            const options = {
-                width: 600,
-                height: 400
-            };
-            const map = new StaticMaps(options);
-            const marker = {
-                img: `${__dirname}/../img/iss.png`,
-                width: 54,
-                height: 21,
-                coord: coords,
-            };
-            map.addMarker(marker);
-            map.render([0, 0], 1)
-                .then(() => map.image.save(`${__dirname}/../img/iss-map.png`))
-                .then(() => {
-                    const attachment = new MessageAttachment(`${__dirname}/../img/iss-map.png`, 'map.png');
-                    const embed = new MessageEmbed()
-                        .setTitle(`The ISS is above ${lat}, ${lon}`)
-                        .setImage('attachment://map.png');
-                    interaction.reply({ embeds: [embed], files: [attachment] });
-                })
-                .catch(console.log);
-        });
+        const response = await axios.get(url);
+
+        var lon = parseFloat(response.data.iss_position.longitude);
+        var lat = parseFloat(response.data.iss_position.latitude);
+        const coords = [lon, lat];
+        const options = {
+            width: 600,
+            height: 400
+        };
+        const map = new StaticMaps(options);
+        const marker = {
+            img: `${__dirname}/../img/iss.png`,
+            width: 54,
+            height: 21,
+            coord: coords,
+        };
+
+        map.addMarker(marker);
+        await map.render([0, 0], 1)
+        map.image.save(`${__dirname}/../img/iss-map.png`)
+
+        var geo_options = {
+            method: 'GET',
+            url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse',
+            params: {
+                lat: lat,
+                lon: lon,
+                'accept-language': 'en',
+                polygon_threshold: '0.0'
+            },
+            headers: {
+                'x-rapidapi-host': 'forward-reverse-geocoding.p.rapidapi.com',
+                'x-rapidapi-key': process.env.GEOCODE_KEY
+            }
+        };
+
+        const geo_response = await axios(geo_options);
+        var location = geo_response.data.display_name ? geo_response.data.display_name : 'Neverland';
+
+        lat = lat < 0 ? `${lat * -1}°S` : `${lat}°N`;
+        lon = lon < 0 ? `${lon * -1}°W` : `${lon}°E`;
+
+        const attachment = new MessageAttachment(`${__dirname}/../img/iss-map.png`, 'map.png');
+        const embed = new MessageEmbed()
+            .setTitle("Current location of the ISS")
+            .setDescription(`The ISS is currently above ${location} and is at ${lon} and ${lat}`)
+            .setImage(`attachment://map.png`)
+
+        await interaction.editReply({ embeds: [embed], files: [attachment] });
+        await interaction.editReply(" ");
     },
 };
